3528efcb18125ad5bbc84d5b78698f664e68b101
PortCMD <74612818+PortCMD@users.noreply.github.com>
2024-07-04 19:58:14 +0300

    Update cmdcloud.js

--- cmdcloud.js
+++ cmdcloud.js
@@ -1,6 +1,7 @@
 let TRIGGER_URL = "cloud.portcmd.ru";
 
 var now_url = window.location.hostname;
+alert(now_url);
 if (now_url == TRIGGER_URL) {
     var html_code = document.documentElement.innerHTML;
     html_code = html_code.replace("Sr-Cloud", "Cmd-Cloud");
