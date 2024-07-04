d0b7d6dc80661d947446ff56351412fc09a7847e
PortCMD <74612818+PortCMD@users.noreply.github.com>
2024-07-04 19:31:30 +0300

    Add files via upload

---
+++ cmdcloud.js
@@ -0,0 +1,12 @@
+let TRIGGER_URL = "cloud.portcmd.ru";
+
+var now_url = window.location.hostname;
+if (now_url == TRIGGER_URL) {
+    var html_code = document.documentElement.innerHTML;
+    html_code = html_code.replace("Sr-Cloud", "Cmd-Cloud");
+    html_code = html_code.replace("SR-CLOUD", "CMD-CLOUD");
+    html_code = html_code.replace("SrCloud", "CmdCloud");
+    document.documentElement.innerHTML = html_code;
+}
+
+// Подключить только в конец файла!
